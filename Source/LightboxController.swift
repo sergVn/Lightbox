8de31444f3de227ecb9bac9cceb2fefb6a37e84b
Serhii Biloshkurskyi <biloshkurskyi.ss@gmail.com>
2022-04-28 01:56:09 +0200

    Added page view.

--- Source/LightboxController.swift
+++ Source/LightboxController.swift
@@ -13,7 +13,7 @@ public protocol LightboxControllerDismissalDelegate: class {
 public protocol LightboxControllerTouchDelegate: class {
 
   func lightboxController(_ controller: LightboxController, didTouch image: LightboxImage, at index: Int)
-  func lightboxController(_ controller: LightboxController, pageViewDidZoom image: LightboxImage, at index: Int)
+    func lightboxController(_ controller: LightboxController, pageViewDidZoom image: LightboxImage, at index: Int, pageView: PageView)
 }
 
 open class LightboxController: UIViewController {
@@ -398,7 +398,7 @@ extension LightboxController: PageViewDelegate {
 
   func pageViewDidZoom(_ pageView: PageView) {
     let duration = pageView.hasZoomed ? 0.1 : 0.5
-    imageTouchDelegate?.lightboxController(self, pageViewDidZoom: images[currentPage], at: currentPage)
+      imageTouchDelegate?.lightboxController(self, pageViewDidZoom: images[currentPage], at: currentPage, pageView: pageView)
     toggleControls(pageView: pageView, visible: !pageView.hasZoomed, duration: duration, delay: 0.5)
   }
 
